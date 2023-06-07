747109e172c37e1662ee9598a1da42734022ba73
danipetro21 <idany346@gmail.com>
2023-06-07 14:35:46 +0200

    add first part of ex

--- src/org/lessons/java/exception/Biblioteca.java
+++ src/org/lessons/java/exception/Biblioteca.java
@@ -1,2 +1,39 @@
-package org.lessons.java.exception;public class Biblioteca {
+package org.lessons.java.exception;
+
+import java.util.Scanner;
+
+public class Biblioteca {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        /*  Libro book = new Libro("Hello world!!", 150, "Daniele", "Mondadori");*/
+
+        System.out.print("Quanti libri vuoi inserire: ");
+        int n = Integer.parseInt(scan.nextLine());
+
+        Libro[] listLibri = new Libro[n];
+
+        for (int i = 0; i < listLibri.length; i++) {
+
+            System.out.print("Inserisci il Titolo del libro: ");
+            String titotlo = scan.nextLine();
+            System.out.print("Inserisci il numero di pagine del libro: ");
+            int nPagine = Integer.parseInt(scan.nextLine());
+            System.out.print("Inserisci l'autore del libro: ");
+            String autore = scan.nextLine();
+            System.out.print("Inserisci l'editore del libro: ");
+            String editore = scan.nextLine();
+
+            Libro book = new Libro(titotlo, nPagine, autore, editore);
+            listLibri[i] = book;
+        }
+
+        for (int i = 0; i < listLibri.length; i++) {
+
+            System.out.println(listLibri[i].toString());
+        }
+
+    }
+
 }
--- src/org/lessons/java/exception/Libro.java
+++ src/org/lessons/java/exception/Libro.java
@@ -1,2 +1,58 @@
-package org.lessons.java.exception;public class Libro {
+package org.lessons.java.exception;
+
+public class Libro {
+
+    private String titolo;
+    private int numeroPagine;
+    private String autore;
+    private String editore;
+
+    public Libro(String titolo, int numeroPagine, String autore, String editore) {
+        this.titolo = titolo;
+        this.numeroPagine = numeroPagine;
+        this.autore = autore;
+        this.editore = editore;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getEditore() {
+        return editore;
+    }
+
+    public void setEditore(String editore) {
+        this.editore = editore;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titolo='" + titolo + '\'' +
+                ", numeroPagine=" + numeroPagine +
+                ", autore='" + autore + '\'' +
+                ", editore='" + editore + '\'' +
+                '}';
+    }
 }
